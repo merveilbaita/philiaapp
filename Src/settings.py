6e667b9e5f0cd1bf0bee447b620867e4010b0af1
Hidden <hidden@MacBook-Air.local>
2025-08-02 00:23:24 +0200

    commit admin

--- Src/settings.py
+++ Src/settings.py
@@ -79,13 +79,16 @@ TIME_ZONE = 'Africa/Lubumbashi'
 USE_I18N = True
 USE_TZ = True
 
-# Static files
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'  # collectstatic y déposera les fichiers
-STATICFILES_DIRS = [BASE_DIR / 'static']  # si tu as des statics en développement
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  # versionnée + gzip
 
-# Auth / login
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.2/howto/static-files/
+
+STATIC_URL = 'static/'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = '/connexion/'
 
 # Default primary key
