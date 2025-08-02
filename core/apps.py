51c7e2e74604ad232244aff481d21f6bc0a6475c
Hidden <hidden@MacBook-Air.local>
2025-08-03 00:56:02 +0200

    correction du fichier apps.py pour l'historique de connexions

--- core/apps.py
+++ core/apps.py
@@ -1,10 +1,13 @@
+# core/apps.py
 from django.apps import AppConfig
-
 
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
 
-def ready(self):
-    import core.signals
+    def ready(self):
+        import core.signals  # noqa: F401
+
+
+
 
